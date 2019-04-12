1f182b22c4121f8a535a2f1a6dd7ba590b9001b8
mayee007 <yelekeri@gmail.com>
2019-04-11 21:02:02 -0400

    added another test case

--- src/test/java/com/myamazon/amazon/AmazonLoginTest.java
+++ src/test/java/com/myamazon/amazon/AmazonLoginTest.java
@@ -9,21 +9,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AmazonLoginTest {
-	   String actualTitle = "";
-	   String expectedTitle = "Amazon.com: Online Shopping for Electronics, "
-	   		+ "Apparel, Computers, Books, DVDs & more";
-	   WebDriver driver;
-
+	String actualTitle = "";
+	String expectedTitle = "Amazon.com: Online Shopping for Electronics, " + "Apparel, Computers, Books, DVDs & more";
+	WebDriver driver;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		
 
- 	}
+	}
 
 	@Before
 	public void setUp() throws Exception {
-		driver = new FirefoxDriver(); 
+		driver = new FirefoxDriver();
 	}
 
 	@After
@@ -34,30 +31,40 @@ public class AmazonLoginTest {
 	@Test
 	public void googleHomePageTest() throws InterruptedException {
 		driver.get("http://amazon.com/");
-        driver.findElement(By.name(("field-keywords"))).sendKeys("Books");
- 		Thread.sleep(1000);
- 		driver.findElement(By.cssSelector((".nav-search-submit"))).click();
- 		Thread.sleep(1000);
+		driver.findElement(By.name(("field-keywords"))).sendKeys("Books");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector((".nav-search-submit"))).click();
+		Thread.sleep(1000);
 
 	}
-        @Test
-         	public void AmazonTitle() throws InterruptedException {
-     		driver.get("http://amazon.com/");
-            actualTitle = driver.getTitle();
-            System.out.println(actualTitle);
-            if (actualTitle.contentEquals(expectedTitle)){
-            	System.out.println("Test case pass");
-            	
-            
-            } else {
-                System.out.println("Test Failed");
-               
-            }
-            	
-				
- 	}
-	
-		
-	
+
+	@Test
+	public void AmazonTitle() throws InterruptedException {
+		driver.get("http://amazon.com/");
+		actualTitle = driver.getTitle();
+		System.out.println(actualTitle);
+		if (actualTitle.contentEquals(expectedTitle)) {
+			System.out.println("Test case pass");
+
+		} else {
+			System.out.println("Test Failed");
+
+		}
+
+	}
+	@Test
+	public void AmazonTitle2() throws InterruptedException {
+		driver.get("http://amazon.com/");
+		actualTitle = driver.getTitle();
+		System.out.println(actualTitle);
+		if (actualTitle.contentEquals(expectedTitle)) {
+			System.out.println("Test case pass");
+
+		} else {
+			System.out.println("Test Failed");
+
+		}
+
+	}
 
 }
